87a0437ca91946f7b3da4789dc3ad7e957c516df
Ahmed Mehamedysuf <mehamedseid002@gmail.com>
2025-11-19 18:14:07 +0300

    product fix try

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -1,8 +1,18 @@
 import { neon } from '@netlify/neon';
+const sql = neon();
 
-export default async () => {
-    const sql = neon();
-    const products = await sql`SELECT * FROM products ORDER BY id`;
+export default async (req) => {
+    const url = new URL(req.url);
+    const id = url.searchParams.get('id');
+
+    let query;
+    if (id) {
+        query = sql`SELECT * FROM products WHERE id = ${id}`;
+    } else {
+        query = sql`SELECT * FROM products ORDER BY id`;
+    }
+
+    const products = await query;
     return new Response(JSON.stringify(products), {
         headers: { 'Content-Type': 'application/json' }
     });
